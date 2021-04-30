bd31ec7c200fbeadbd4fd4fc3fcc9d810991d90a
okada asako <asako_okada@waku-2.com>
2021-04-30 12:16:08 +0900

    add テスト用サイト作成

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+// moduleを付けないとcssモジュールとして読み込んでくれない！
+import styled from '../../styles/FirstPost.module.css'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div className={styled.abWrapper}>
+        <h1 className={styled.firstTitle}>First Post</h1>
+          <p>ABテストで下記を変える</p>
+          <div className={styled.abBlock}></div>
+          <h2>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </h2>
+        </div>
+    </Layout>
+  )
+}
